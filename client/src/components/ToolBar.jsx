17d66f5946831e903e2f12ae9ef2d3cf3624347f
Alexander Russanow <alexander.russanow@gmail.com>
2021-09-01 20:23:53 +0200

    update

--- client/src/components/ToolBar.jsx
+++ client/src/components/ToolBar.jsx
@@ -6,6 +6,7 @@ import Brush from "../tools/Brush";
 import Circle from "../tools/Circle";
 import Rect from "../tools/Rect";
 import Eraser from '../tools/Eraser'
+import Line from "../tools/Line";
 
 const ToolBar = () => {
   const colorChanger = (e) => {
@@ -28,7 +29,7 @@ const ToolBar = () => {
       <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
       <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}></button>
       <button className="toolbar__btn eraser" onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}></button>
-      <button className="toolbar__btn line"></button>
+      <button className="toolbar__btn line" onClick={() => toolState.setTool(new Line(canvasState.canvas))}></button>
       <input onClick={(e) => colorChanger(e)} style={{ marginLeft: 10 }} type="color" />
       <button className="toolbar__btn undo" onClick={() => canvasState.undo()}></button>
       <button className="toolbar__btn redo" onClick={() => canvasState.redo()}></button>
--- server/index.js
+++ server/index.js
@@ -13,13 +13,16 @@ app.ws('/', (ws, req) => {
          case "CONNECTION":
             connectionHandler(ws, msg)
             break
-         case "DRAW": 
+         case "DRAW":
             broadcastConnection(ws, msg)
             break
 
       }
    })
 })
+
+
+
 
 app.listen(PORT, () => console.log('server start on port: ' + PORT))
 
@@ -35,3 +38,24 @@ const broadcastConnection = (ws, msg) => {
       }
    })
 }
+
+// app.post('/image', (req, res) => {
+//    try {
+//       const data = req.body.img.replace(`data:image/png;base64,`, '')
+//       fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
+//       return res.status(200).json({ message: "Loaded" })
+//    } catch (e) {
+//       console.log(e)
+//       return res.status(500).json('error')
+//    }
+// })
+// app.get('/image', (req, res) => {
+//    try {
+//       const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+//       const data = `data:image/png;base64,` + file.toString('base64')
+//       res.json(data)
+//    } catch (e) {
+//       console.log(e)
+//       return res.status(500).json('error')
+//    }
+// })
